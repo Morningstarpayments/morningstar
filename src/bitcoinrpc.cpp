bdcd55e55e2056cc079aae7784a3e9c56f0fd439
Morningstarpayments <31256375+Morningstarpayments@users.noreply.github.com>
2017-09-07 18:03:01 -0600

    Update bitcoinrpc.cpp

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 22789 : 94622;
+    return GetBoolArg("-testnet", false) ? 22789 : 29086;
 }
 
 Object JSONRPCError(int code, const string& message)
