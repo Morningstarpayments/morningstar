73e155acf4ed80ccb36cf0302b2b316eb4f9c435
Morningstarpayments <31256375+Morningstarpayments@users.noreply.github.com>
2017-09-07 18:15:54 -0600

    Update protocol.h

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 22788 : 94722;
+    return testnet ? 22788 : 29186;
 }
 
 
